8af01aa48c41a0393f81db7ec2b1d71807abed1b
FevenBelay23 <91492132+FevenBelay23@users.noreply.github.com>
2023-01-22 00:24:34 +0300

    Time: 23 ms (92.55%), Space: 14 MB (87.99%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -5,8 +5,8 @@ class Solution(object):
         :rtype: List[str]
         """
         answer = []
-        for i in range(1,n+1):
-            if i % 3 == 0 and i % 5 == 0:
+        for i in range(1, n+1):
+            if i % 5 == 0 and i % 3 == 0:
                 answer.append("FizzBuzz")
             elif i % 3 == 0:
                 answer.append("Fizz")
@@ -15,4 +15,4 @@ class Solution(object):
             else:
                 answer.append(str(i))
         return answer
-                
+        
